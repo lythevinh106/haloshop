b08f639bd72c809c8ece066e7bf7fced9abfec4d
lythevinh106@gmail.com <lythevinh106@gmail.com>
2023-02-02 14:17:57 +0700

    change url

--- src/constanst/UrlContanst.jsx
+++ src/constanst/UrlContanst.jsx
@@ -4,5 +4,5 @@ import PropTypes from 'prop-types';
 export const baseUrlDemo = "https://api.ezfrontend.com";
 
 
-export const baseUrl = "http://127.0.0.1:8000/api"
+export const baseUrl = "http://api-x.canv.space/api"
 
