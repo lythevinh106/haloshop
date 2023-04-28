124912ed714c88644953ed01b41326ec8878525a
lythevinh106@gmail.com <lythevinh106@gmail.com>
2023-04-28 22:47:08 +0700

    up second

--- src/constanst/UrlContanst.jsx
+++ src/constanst/UrlContanst.jsx
@@ -4,5 +4,5 @@ import PropTypes from 'prop-types';
 export const baseUrlDemo = "https://api.ezfrontend.com";
 
 
-export const baseUrl = "https://benat.net/api"
+export const baseUrl = "https://sv1.manhbf.site/api"
 
