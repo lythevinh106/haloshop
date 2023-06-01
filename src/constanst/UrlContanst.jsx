58388e2267fc8b0cf045c96511670b553cb33709
lythevinh106@gmail.com <lythevinh106@gmail.com>
2023-06-01 16:52:54 +0700

    1/6/2023

--- src/constanst/UrlContanst.jsx
+++ src/constanst/UrlContanst.jsx
@@ -7,5 +7,6 @@ export const baseUrlDemo = "https://api.ezfrontend.com";
 // export const baseUrl = "https://sv1.manhbf.site/api"
 
 
-export const baseUrl = "http://127.0.0.1:8000/api";
+// export const baseUrl = "http://127.0.0.1:8000/api";
+export const baseUrl = "https://lythevinh.duckdns.org/api";
 
